fbb58fb06dcc4d95de9af2bd5410b316d4a94ab0
SE.Wadea <wadeayawanseh@gmail.com>
2025-07-14 15:43:29 +0300

    Finally The hexadecimal question Solution

--- ex10/ft_putstr_non_printable.c
+++ ex10/ft_putstr_non_printable.c
@@ -1,35 +1,55 @@
 #include <unistd.h>
 
-void ft_putchar(char c) {
+void ft_putchar(char c)
+{
     write(1, &c, 1);
 }
 
-void ft_putstr(char *str) {
-    int i = 0;
-    while (str[i])
-        write(1, &str[i++], 1);
-}
-
-void ft_putnbr(int n) {
-    if (n > 9)
-        ft_putnbr(n / 10);
-    ft_putchar(n % 10 + '0');
-}
-
-void ft_putarr(int *n, int size){
-    int i = 0;
-    while (i < size)
-    {
-        ft_putnbr(n[i]);
-        i++;
-        ft_putchar('\n');
-    }
-    
-}
-
-
-
-int main(void)
+int ft_str_is_printable(char *str)
 {
-    ft_putstr("Not Solved Yet");
+    int     i;
+    int     x;
+    int    flag;
+
+    i = 0;
+    flag = 0;
+    while (str[i])
+    {
+        x = (int)str[i];
+        if ((x < 32) || (x > 126))
+        {
+            return (0);
+            flag = 1;
+        }
+        i++;
+    }
+    if(flag != 1)
+        return (1);
+}
+
+
+
+void ft_putstr_non_printable(char *str)
+{
+    int i = 0;
+    int c;
+    int high;
+    int low;
+    char *hex = "0123456789abcdef";
+
+    while (str[i])
+    {
+        c = (int)str[i];
+        if (c >= 32 && c <= 126)
+            ft_putchar(str[i]);
+        else
+        {
+            ft_putchar('\\');
+            high = c / 16;
+            low = c % 16;
+            ft_putchar(hex[high]);
+            ft_putchar(hex[low]);
+        }
+        i++;
+    }
 }
